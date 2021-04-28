128e03eef0397170c8129d7c9c248f021c31ea5f
= <karmamonk@nextmeme.ch>
2021-04-28 14:33:58 +0200

    Updated Design

--- diflashcard.js
+++ diflashcard.js
@@ -7,9 +7,6 @@ import button from "/component/dibutton.js";
 const limit = 500;
 let offset =0;
 
-
-
-// 
 const color = {
 	gray100 : '#FBFBFB',
 	gray200 : '#F8F8F8',
@@ -27,24 +24,23 @@ const color = {
 	neoGreen: '#94FF57',
 }
 
-
-
-	
 b.css('.box',`
-	border-left: 7px solid ${color.green};
+	border-left: 8px solid ${color.green};
+	border-right: 1px solid ${color.gray400};
 	padding: 0;
 	margin: 0;
-	width: 100%;
+	width: 99%;
 	background-color: ${color.gray200};
 `)
-
-
 
 b.css(".header",`
 	display: flex;
 	justify-content: space-between;
-	padding: 0.625rem;
+	padding: 0 1.25rem 0 1.25rem;
 	margin: 0;
+	border-color: #B6B6B6;
+	border-style: solid;
+	border-width: 1px 0 1px 0;
 `)
 
 b.css(".headerLeft",`
@@ -52,7 +48,7 @@ b.css(".headerLeft",`
 	justify-content: flex-start;
 	align-items: center;		
 	width: 33%;
-	font-size: 0.875rem;
+	font-size: 1rem;
 `)
 
 b.css(".headerCenter",`
@@ -60,7 +56,7 @@ b.css(".headerCenter",`
 	justify-content: center;
 	align-items: center;		
 	width: 33%;
-	font-size: 0.875rem;
+	font-size: 1rem;
 `)
 
 b.css(".headerRight",`
@@ -68,7 +64,7 @@ b.css(".headerRight",`
 	justify-content: flex-end;
 	align-items: center; 
 	width: 33%;
-	font-size: 0.875rem;
+	font-size: 1rem;
 `)
 
 
@@ -76,8 +72,9 @@ b.css(".headerRight",`
 b.css(".footer",`
 	display: flex;
 	justify-content: space-between;
-	padding: 0.625rem;
+	padding: 0.625rem 1.25rem 0.625rem 1.25rem;
 	margin: 0;
+	border-bottom: 1px solid ${color.gray400};
 `)
 
 b.css(".footerLeft",`
@@ -92,7 +89,7 @@ b.css(".footerCenter",`
 	justify-content: center;
 	display: flex;
 	align-items: center;		
-	width: 40%;
+	width: 20%;
 	font-size: 0.875rem;
 
 `)
@@ -109,7 +106,7 @@ b.css(".footerRight",`
 
 b.css(".contentBox",`
 	overflow: visible;
-	padding: 0.625rem;
+	padding: 1.25rem 1.25rem 0.625rem 1.25rem;
 	display: table outside;
 	font-size: 0.875rem;
 	color: ${color.gray400};
@@ -122,12 +119,12 @@ b.css(".contentLabel",`
 `)
 
 b.css(".textArea",`
-	width: 100%;
+	width: 99.5%;
 	resize: none;
 	padding: 0.625rem;
 	padding-top: 1.25rem;
 	outline: none;
-	border: 1px solid #F8F8F8;
+	border: 1px solid ${color.gray300};
 	background: #FFFFFF;
 	color: black;
 	font-size: 1.25rem;
@@ -136,23 +133,30 @@ b.css(".textArea",`
 b.css(".textArea:focus",`border: 1px solid #00FF93;`)
 
 b.css(".textDiv",`
-	width: 100%;
+	width: 98%;
+	height: 8rem;
 	resize: none;
 	padding: 0.625rem;
 	padding-top: 1.25rem;
 	outline: none;
-	border: 1px solid #F8F8F8;
+	border: 1px solid ${color.gray300};
 	background: #FFFFFF;
 	color: black;
 	font-size: 1.25rem;
 	text-align: center;
 `)
 
-/*
-b.css("",`
+b.css(".turnAnim",`
+	transition: all 1s ease-in;
+	transform: rotateY(90deg);
+`)
 
-*/
-
+b.css(".turnAnim2",
+	b.$animate('1s ease-out', {
+		from: b.transform('rotateY(-90deg)').style,
+		to: b.transform('rotateY(0deg)').style
+  })
+)
 
 function getRandomInt(max) {
 	return Math.floor(Math.random() * Math.floor(max));
@@ -160,6 +164,8 @@ function getRandomInt(max) {
 
 
 export const flashcard = ({query, store, info}) => {
+	let turnAnim = false;
+	let turnAnim2 = false;
 
 	// default values
 	query = query.map(q => Object.assign({
@@ -175,55 +181,62 @@ export const flashcard = ({query, store, info}) => {
 	let overLearning = 2;  // n mal richtig beantworten
 	let fails = 0;
 	let successes = 0;
-	let mode = 'hidden';  // 3 Modi: "edit",  "hidden" (play), "show" (play)
+	let mode = 'playQuestion';  // 3 Modi: "edit",  "playQuestion", "playAnswer"
 	let type = 'q';       // 2 Typen: q (question), a (answer)
 	let i = 0;
 	let cards = store()?.cards || query()?.cards || [{"q":"Your First Question","a":"Your First Answer"}];
 	console.log(cards)
 
 //	const enabledReset = () => store()?.text != query()?.text;
-	const onClickedChangeMode = () => { 
-		console.log("changeMode")
+	const changeMode = () => { 
 		if (mode==='edit'){
-			mode='hidden';
+			mode='playQuestion';
 		} else {
 			mode='edit';
 		}	
 	 }
 
-	const onClickedStandalone = () => {
+	const maximize = () => {
+		// FIXME
+	}
+
+	const standalone = () => {
 		window.open('https://gem.omlu.ch/app#0='+compress({
 			a: 'diflashcard', q: JSON.stringify(query()), s: store()
 		}, "_"))
 	}
 
-	const onClickedFullScreen = () => {}
 	
-	const onClickedPrevious = () => {
-		i--;
-		if (i<0) {i= cards.length-1};
-	}
-
-	const onClickedNext = () => {
+	const nextCard = () => {
 		i++;
 		if (i>=cards.length) {i= 0};
 	}
 
-	const onClickedShowMe = () => {
-		mode='show';
+	const previousCard = () => {
+		i--;
+		if (i<0) {i= cards.length-1};
 	}
 
-	const onClickedFail = () => {
-		fails++;
-		let tries = cards.length*cards.length*10;
-		do {
-			i = getRandomInt(cards.length);
-			tries--;
-		} while (cards[i].n<=overLearning && tries>0)
-		mode='hidden';
+	const turnCard = () => {
+		turnAnim = true; 
 	}
 
-	const onClickedSuccess = () => {
+	const turnCardEnd = () => {
+		console.log("turnCardEnd")
+
+		if (turnAnim) { 
+			turnAnim = false; 
+			turnAnim2 = true;
+		} else {
+			console.log("Anim2")
+			turnAnim2 = false;
+		}
+
+		mode='playAnswer';
+	}
+
+
+	const successed = () => {
 		successes++;
 		if (cards[i].n) {cards[i].n++} else {cards[i].n = 1};
 		let tries = cards.length*cards.length*10;
@@ -233,22 +246,29 @@ export const flashcard = ({query, store, info}) => {
 			console.log(cards[i].n)
 		} while (cards[i].n>=overLearning && tries>0)
 		if (tries==0) label = "YEEESSS .. YOU DID IT"
-		mode='hidden';
+		mode='playQuestion';
 	}
 	
+	const failed = () => {
+		fails++;
+		let tries = cards.length*cards.length*10;
+		do {
+			i = getRandomInt(cards.length);
+			tries--;
+		} while (cards[i].n<=overLearning && tries>0)
+		mode='playQuestion';
+	}
+
+	const addCard = () => {
+		cards.push({"q":"Type your Question", "a":"Type your Answer"}); 
+		i=cards.length-1;
+	}
 	
-	const onClickedDelete = () => {
+	const deleteCard = () => {
 		cards.splice(i, 1); 
 		i--;
 		if (i<0) {i= cards.length-1}
 	}
-
-	const onClickedAdd = () => {
-		cards.push({"q":"Type your Question", "a":"Type your Answer"}); 
-		i=cards.length-1;
-	}
-
-
 
 	// ============================================
 	//
@@ -257,10 +277,16 @@ export const flashcard = ({query, store, info}) => {
 	const textfield = (mode, type, content) => 
 		m('div.contentBox',
 		m('div.contentLabel', (type === 'q') ? 'Front': 'Back'),            
-		m(((mode === 'edit') ? 'textarea.textArea' : 'div.textDiv'), 
-			(mode === 'show') ? { innerText: (type === 'q') ? content.q : content.a,} :
-			(mode === 'hidden') ? { innerText: "  ??? " }:
-			{
+		m(((mode === 'edit') ? 'textarea.textArea' : 'div.textDiv') 
+			+ (turnAnim ? ".turnAnim" : "")
+			+ (turnAnim2 ? ".turnAnim2": ""), 
+			(mode === 'playAnswer') 
+			? { 
+				innerText: (type === 'q') ? content.q : content.a,
+				ontransitionend: turnCardEnd,
+				onanimationend: turnCardEnd
+			} 
+			: {
 				value: (type === 'q') ? content.q : content.a,
 				oncreate: ({dom}) => {
 					var offset = dom.offsetHeight - dom.clientHeight + 3;
@@ -278,17 +304,14 @@ export const flashcard = ({query, store, info}) => {
 					} else {
 						content.a = t.value.substr(0, limit);
 					}				
-				})
+				})				
 			} 
 		)
 	);
 	
 
 
-	const nOfAll = ()=> m('span'+b`
-		padding: 0.625rem;
-		font-size: 0.875rem;`, ((i+1) + '/'+ cards.length) 
-	);
+	const nOfAll = ()=> m('span', ((i+1) + '/'+ cards.length));
 	
 	const spacer = () => m('span'+b`width:4px;`)
 
@@ -298,29 +321,25 @@ export const flashcard = ({query, store, info}) => {
     //==================================================                                          
 	const playView = () => m('div.box', 
 		m('div.header',
-			m('div.headerLeft', 
-				m(button, {id: 'play' , text: 'Edit Deck', func: onClickedChangeMode})
-			),
-			m('div.headerCenter', label + ' Deck'),
+			m('div.headerLeft', query().label + ' Deck'),
+			m('div.headerCenter', nOfAll()),
 			m('div.headerRight',  
-				nOfAll(), 
-				m(button, {id: 'fullscreen' , func: onClickedFullScreen}),
+				m(button, {id: 'maximize' , onclick: maximize}),
 				spacer(),
-				m(button, {id: 'standalone' , func: onClickedStandalone})
+				m(button, {id: 'standalone' , onclick: standalone})
 			)
 		),
 		
-		textfield('show', 'q', cards[i]),
-		textfield(mode, 'a', cards[i]),
+		(mode==='playQuestion') ? textfield('playAnswer', 'q', cards[i]) : textfield('playAnswer', 'a', cards[i]),
 
 		m('div.footer',
-			m('div.footerLeft'),
+			m('div.footerLeft', m(button, {id: 'play' , text: 'Edit Deck', onclick: changeMode}) ),
 			m('div.footerCenter'),
 			m('div.footerRight', 	
-				(mode==='hidden') ? m(button,{id: 'forward', text: 'Show me', func: onClickedShowMe}) : '',
-				(mode==='show') ? m(button,{id: 'success', text: 'Yes, I got it right', func: onClickedSuccess}):'', 
-				(mode==='show') ? spacer():'', 
-				(mode==='show') ? m(button,{id: 'fail', text: 'No, next', func: onClickedFail}):'' 
+				(mode==='playQuestion') ? m(button,{id: 'turn', text: 'Turn', onclick: turnCard}) : '',
+				(mode==='playAnswer') ? m(button,{id: 'fail', text: 'Wrong', onclick: failed}):'', 
+				(mode==='playAnswer') ? spacer():'', 
+				(mode==='playAnswer') ? m(button,{id: 'success', text: 'Correct', onclick: successed}):''
 			)
 		)
 	)
@@ -330,13 +349,12 @@ export const flashcard = ({query, store, info}) => {
     //==================================================  
 	const editView = () => m('div.box', 
 		m('div.header',
-			m('div.headerLeft', m(button,{id: 'play', text: 'Play Deck', func: onClickedChangeMode})),
-			m('div.headerCenter', query().label + ' Deck'),
+			m('div.headerLeft', query().label + ' Deck'),
+			m('div.headerCenter', nOfAll()),
 			m('div.headerRight', 
-				nOfAll(), 
-				m(button,{id:'fullscreen' , func: onClickedFullScreen}), 
+				m(button,{id:'maximize' , onclick: maximize}), 
 				spacer(),
-				m(button,{id:'standalone' ,func: onClickedStandalone})
+				m(button,{id:'standalone' ,onclick: standalone})
 			)
 		),
 		
@@ -345,21 +363,23 @@ export const flashcard = ({query, store, info}) => {
 		
 		m('div.footer',
 			m('div.footerLeft',
-				m(button,{id:'delete', text: 'Delete Card', func: onClickedDelete}),
+				m(button,{id: 'play', text: 'Play Deck', onclick: changeMode}),
 				spacer(),
-				m(button,{id:'add', text: 'Add Card', func: onClickedAdd}) 
+				m(button,{id:'delete', text: 'Delete Card', onclick: deleteCard}),
+				spacer(),
+				m(button,{id:'add', text: 'Add Card', onclick: addCard}) 
 			),
 			m('div.footerCenter'),
 			m('div.footerRight', 
-				m(button,{id:'back' , func: onClickedPrevious}),
+				m(button,{id:'back' , onclick: previousCard}),
 				spacer(),
-				m(button,{id:'forward', func: onClickedNext})
+				m(button,{id:'forward', onclick: nextCard})
 			)
 		)
 	)
 		
 	return { 
-		view: () => (mode==='hidden' || mode==='show') ? playView() : editView()
+		view: () => (mode==='playQuestion' || mode==='playAnswer') ? playView() : editView()
 	}
 }
 
